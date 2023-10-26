39be87e00dabfb3f242e2a72bbe15d0ce5deb116
Mike Barkmin <mike@barkmin.eu>
2023-10-26 06:18:56 +0000

    änderung

--- MyProgram.java
+++ MyProgram.java
@@ -8,15 +8,17 @@ class MyProgram {
   private Rectangle rechteck;
 
   public MyProgram() {
-    ansicht = new View(600, 600, "My Program");
-    rechteck = new Rectangle(10, 10, 480, 390);
+    ansicht = new View(300, 300, "My Program");
+    rechteck = new Rectangle(10, 10, 180, 190);
 
     while(true) {
       rechteck.turn(1);
       ansicht.wait(100);
 
+      rechteck.setColor(Color.ORANGE);
+
       if(ansicht.keyUpPressed()) {
-        rechteck.setColor(Color.RED);
+        rechteck.setColor(Color.GRAY);
       } else if (ansicht.keyDownPressed()) {
         rechteck.setColor(Color.BLUE);
       }
